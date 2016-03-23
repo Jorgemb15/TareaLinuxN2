eea2570980ceb2763a99b31928ca7ce74e690459
Jorge <jorgemb15@hotmail.es>
2016-03-22 20:57:49 -0600

    finalizacion de clase

--- hojasdeexcel/src/graficar.sh
+++ hojasdeexcel/src/graficar.sh
@@ -15,7 +15,16 @@ for archivo in ` find $DATOS -name "*.xls"`
 do
 	echo "Procesando Archivo $archivo"
 	xls2csv	$archivo > $SALIDA_DATOS/datos-$M.scv
-	let M=$M+1
+	let M=M+1
 done 2>error1.log
+
+M=0
+
+for archivo in ` find $SALIDA_DATOS -name "*.csv"`
+do
+	echo "Dando formato al archivos de Datos $archivo"
+	cat $archivo | awk -F "\",\"" '{print $1 " " $2 " " $3 " " $4 " " $5}' | grep -v Sensor | sed '1,$ s/"//g' > $SALIDA_DATOS/datos-$M.out
+	let M=M+1
+done 2>error2.log
 
 
